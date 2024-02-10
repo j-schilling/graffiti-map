07ff2a8b025da113f2b656f9e538aec323416bc2
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-10 02:21:39 +0100

    adding marker

--- src/app/components/map/Map.js
+++ src/app/components/map/Map.js
@@ -2,15 +2,15 @@
 
 import L from "leaflet";
 
-// import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
-// import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png";
+import MarkerIcon from "/node_modules/leaflet/dist/images/marker-icon.png";
+import MarkerShadow from "/node_modules/leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
 
 const Map = () => {
   const [coord, setCoord] = useState([51.505, -0.09]);
-
+  console.log("coord:", coord);
   const SearchLocation = () => {
     return (
       <div className="search-location">
@@ -24,6 +24,7 @@ const Map = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
           setCoord([position.coords.latitude, position.coords.longitude]);
+          console.log(position);
         });
       } else {
         console.log("Geolocation is not supported by this browser.");
@@ -54,25 +55,25 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* 
+
         <Marker
           icon={
             new L.Icon({
-              //   iconUrl: MarkerIcon.src,
-              //   iconRetinaUrl: MarkerIcon.src,
+              iconUrl: MarkerIcon.src,
+              iconRetinaUrl: MarkerIcon.src,
               iconSize: [25, 41],
               iconAnchor: [12.5, 41],
               popupAnchor: [0, -41],
-              //   shadowUrl: MarkerShadow.src,
+              shadowUrl: MarkerShadow.src,
               shadowSize: [41, 41],
             })
           }
-          position={[51.505, -0.09]}
+          position={coord}
         >
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker> */}
+        </Marker>
       </MapContainer>
     </div>
   );
