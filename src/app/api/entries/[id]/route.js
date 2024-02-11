be5276ba380f4bcfaf44af11274c1a5ec472b340
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-11 19:11:54 +0100

    adding get requests for all entries and entries by ID

--- src/app/api/entries/[id]/route.js
+++ src/app/api/entries/[id]/route.js
@@ -1,12 +1,8 @@
 import { getEntryById } from "../../../services/entryServices";
-import { NextResponse } from "next/server";
-
-// export default function handler(req, res) {
-//   const { id } = req.query;
-//   res.status(200).json(getEntryById(id));
-// }
-export async function GET(request) {
-  const { id } = request.query;
+import { NextRequest, NextResponse } from "next/server";
+export async function GET(req, { params }) {
+  const { id } = params;
   const entry = await getEntryById(id);
+
   return NextResponse.json({ entry }, { status: 200 });
 }
--- src/app/api/entries/route.js
+++ src/app/api/entries/route.js
@@ -1,5 +1,5 @@
 import { getAllEntries } from "../../services/entryServices";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request) {
   const entries = await getAllEntries();
