1831711449d6b7eee0bdddce598cd89e469575b0
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-12 10:51:47 +0100

    use params for detail page

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,25 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
-import { useRouter } from "next/navigation";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
-export default function DetailPage() {
-  // const searchParams = useSearchParams();
-  // console.log("searchParams", searchParams);
+export default function DetailPage({ params }) {
+  console.log("params", params);
 
-  // const url = new URL(request.url);
-  // console.log("url", url);
+  const { id } = params;
 
-  // const { id } = searchParams.id;
-  // console.log("id", id);
-  const router = useRouter();
-  console.log("router", router);
-  // const { id } = router.query;
-  const id = 1;
   const { data, error, isLoading } = useSWR(`/api/entries/${id}`, fetcher);
   console.log("data", data);
   if (error) return <div>failed to load</div>;
