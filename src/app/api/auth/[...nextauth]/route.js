fd95d0be458001b0c797ae4ace4161f3e2c55cad
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-17 13:16:28 +0100

    starting auth process

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+const handler = NextAuth({});
+
+export { handler as GET, handler as POST };
