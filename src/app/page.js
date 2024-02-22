4a57b18651f2f6b51ac9865bfcdbb645530e4927
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-22 13:14:02 +0100

    put logo in component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import SignInButton from "./components/signinbutton/SignInButton";
-import Logo from "../../public/icons/GraffitiMap_2.png";
-import Image from "next/image";
+import GraffitiMapLogo from "./components/graffitimaplogo/GraffitiMapLogo";
 
 export default function Home() {
   return (
     <main>
-      <Image src={Logo} width={320} height={133.5} alt="Logo of Graffiti Map" />
+      <GraffitiMapLogo width={320} height={133.5} />
       <h1>Hi Graffiti Fans</h1>
       <SignInButton />
     </main>
