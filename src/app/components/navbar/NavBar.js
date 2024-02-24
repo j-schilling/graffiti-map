c44c9c8e935ba3f25ea1ce007362759fa5887c29
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-24 23:35:46 +0100

    styling the navbar and adding icons to it

--- src/app/components/navbar/NavBar.js
+++ src/app/components/navbar/NavBar.js
@@ -2,33 +2,47 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+import createIcon from "../../../../public/icons/create_GraffitiMap.png";
+import homeIcon from "../../../../public/icons/Haus_GraffitiMap.png";
+import mapIcon from "../../../../public/icons/Marker_GraffitiMap.png";
+import personalIcon from "../../../../public/icons/Nutzer_GraffitiMap.png";
 
 export default function NavBar() {
   const Menus = [
-    { name: "Home", href: "/" },
-    { name: "Map", href: "/map" },
-    { name: "Create", href: "/create" },
-    { name: "Personal", href: "/personal" },
+    { name: "Home", href: "/", src: homeIcon },
+    { name: "Map", href: "/map", src: mapIcon },
+    { name: "Create", href: "/create", src: createIcon },
+    { name: "Personal", href: "/personal", src: personalIcon },
   ];
 
   const [active, setActive] = useState(0);
 
   return (
-    <div className="bg-gwhitedark	 h-14 fixed bottom-0 w-full">
-      <ul className="flex relative">
+    <div className="bg-white h-14 border-t fixed bottom-0 w-full">
+      <ul className="flex justify-evenly">
         {Menus.map((menu, i) => (
-          <li key={i} className="w-20">
+          <li key={i} className="">
             <Link
               href={menu.href}
-              className="flex flex-col text-center p-2 cursor-pointer"
-              onClick={() => setActive(i)}
+              className="flex flex-col items-center p-2 cursor-pointer"
+              // onClick={() => setActive(i)}
             >
+              <Image
+                src={menu.src}
+                width={25}
+                height={25}
+                alt={`${menu.name} icon`}
+              />
               <span
-                className={` ${
-                  active === i
-                    ? " duration-700 opacity-100 font-bold"
-                    : "opacity-40"
-                } `}
+                className="text-xs"
+
+                // className={` ${
+                //   active === i
+                //     ? " duration-700 opacity-100 font-bold"
+                //     : "opacity-40"
+                // } `}
               >
                 {menu.name}
               </span>
