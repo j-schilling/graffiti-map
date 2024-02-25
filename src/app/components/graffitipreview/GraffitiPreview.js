ef8cf4fc1ba11442138acc9005d37b8e23526674
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-25 18:12:52 +0100

    rendering graffitis

--- src/app/components/graffitipreview/GraffitiPreview.js
+++ src/app/components/graffitipreview/GraffitiPreview.js
@@ -1,13 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function GraffitiPreview({ graffitidata }) {
   return (
-    <Image
-      src={graffitidata.images[0]}
-      width={200}
-      height={100}
-      placeholder="blur"
-      alt="image of a graffiti piece"
-    />
+    <section className="w-full h-full border-black border rounded shadow-lg white">
+      <Link href={`/${graffitidata._id}`}>
+        <div>
+          <Image
+            src={graffitidata.images[0]}
+            width={200}
+            height={100}
+            alt="image of a graffiti piece"
+          />
+        </div>
+        <div className="text-left h-full flex flex-col gap-2 p-3">
+          <p className=" 	">{graffitidata.location}</p>
+        </div>
+      </Link>
+    </section>
   );
 }
