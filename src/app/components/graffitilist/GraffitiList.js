b7886879971859d36380c6640318a57a4cccb92c
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-26 11:58:57 +0100

    moving Session Provider outside body

--- src/app/components/graffitilist/GraffitiList.js
+++ src/app/components/graffitilist/GraffitiList.js
@@ -6,7 +6,7 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function GraffitiList() {
   const { data, error, isLoading } = useSWR("/api/graffitis", fetcher);
-  console.log("datag", data);
+
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
 
