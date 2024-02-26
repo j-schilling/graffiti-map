965e7dabbe3552d1f36e5bc9247607ff51e26bd3
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-26 11:34:25 +0100

    changing session syntax

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -8,39 +8,44 @@ import GraffitiForm from "../components/graffitiform/GraffitiForm.js";
 import GraffitiMapLogo from "../components/graffitimaplogo/GraffitiMapLogo";
 
 export default function CreateEntryPage() {
-  const [loadingAddGraffiti, setLoadingAddGraffiti] = useState(false);
-
   const session = useSession();
-  const creator = session.data?.user?.id;
-
   const router = useRouter();
-
-  async function AddGraffiti(entryData) {
-    setLoadingAddGraffiti(true);
-    const response = await fetch("/api/graffitis", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(entryData),
-    });
-    if (!response.ok) {
-      alert(
-        "Ooops, something went wrong. Your graffiti was not added :( Please check, if longitude and latitude are entered in the required format. Thanks!"
-      );
-      console.log(error);
-    }
-    if (response.ok) {
-      setLoadingAddGraffiti(false);
-      router.push("/map");
-      alert("Yes! You have added this graffiti. Check it out on the map now:");
-    }
-  }
+  const [loadingAddGraffiti, setLoadingAddGraffiti] = useState(false);
 
   if (session.status === "loading") {
     return null;
   }
   if (session.status === "authenticated") {
+    console.log("session on create", session);
+    const creator = session.data?.user?.id;
+    console.log("session.data", session.data);
+    console.log("session.data.user", session.data.user);
+    console.log("creator", creator);
+
+    async function AddGraffiti(entryData) {
+      setLoadingAddGraffiti(true);
+      const response = await fetch("/api/graffitis", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(entryData),
+      });
+      if (!response.ok) {
+        alert(
+          "Ooops, something went wrong. Your graffiti was not added :( Please check, if longitude and latitude are entered in the required format. Thanks!"
+        );
+        console.log(error);
+      }
+      if (response.ok) {
+        setLoadingAddGraffiti(false);
+        router.push("/map");
+        alert(
+          "Yes! You have added this graffiti. Check it out on the map now:"
+        );
+      }
+    }
+
     return (
       <main className=" w-full flex flex-col gap-4 p-4 items-center">
         <GraffitiMapLogo width={160} height={66.75} />
