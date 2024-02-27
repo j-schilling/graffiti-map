a2a6e424d6d5bcc18fe57e083dda201c0aa2bbb8
Janosch Schilling <janosch.schilling@gmail.com>
2024-02-27 03:48:08 +0100

    clean up logs

--- src/app/components/graffitilist/GraffitiListPersonal.js
+++ src/app/components/graffitilist/GraffitiListPersonal.js
@@ -14,9 +14,7 @@ export default function GraffitiListPersonal({ creator }) {
   const graffitisAddedByYou = data.entries.filter(
     (entry) => entry.creator === creator
   );
-  console.log("creator in GraffitiListPersonal", creator);
-  console.log("data", data.entries);
-  console.log({ graffitisAddedByYou });
+
   return (
     <section className="flex flex-col gap-4 w-full">
       <h1 className="font-bold text-xl text-center">Added by me:</h1>
